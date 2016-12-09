27383691f8f5887253f3d11f8ea2b2f8f96e6686
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2016-12-09 02:39:07 +0000

    Added first failing test - Non Empty

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,19 @@
 import Test.QuickCheck
 import DiamondKata
 
+newtype AlphaChar = AlphaChar { unAlphaChar :: Char }
+
+instance Arbitrary AlphaChar where
+  arbitrary = AlphaChar <$> genAlphaChar
+
+instance Show AlphaChar where
+  show ch = show $ unAlphaChar ch
+
+genAlphaChar :: Gen Char
+genAlphaChar = elements $ ['a'..'z'] ++ ['A'..'Z']
+
+testNonEmpty :: AlphaChar -> Bool
+testNonEmpty (AlphaChar char) = length (diamondKata char) /= 0
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = mapM_ quickCheck [testNonEmpty]
