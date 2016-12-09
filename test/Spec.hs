10021f8385e64f295bba6f22cbcac40a58d74d5e
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2016-12-09 04:11:56 +0000

    Created Correct Shrink

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,20 @@
-import Data.Char (ord, toLower)
+import Data.Char (ord, chr, toUpper)
 import Test.QuickCheck
 import DiamondKata
+
+genAlphaChar :: Gen Char
+genAlphaChar = elements $ ['a'..'z'] ++ ['A'..'Z']
 
 newtype AlphaChar = AlphaChar { unAlphaChar :: Char }
 
 instance Arbitrary AlphaChar where
   arbitrary = AlphaChar <$> genAlphaChar
+  shrink (AlphaChar 'A') = []
+  shrink (AlphaChar 'a') = []
+  shrink (AlphaChar char) = [(AlphaChar . chr) ((ord . toUpper) char - 1)]
 
 instance Show AlphaChar where
   show ch = show $ unAlphaChar ch
-
-genAlphaChar :: Gen Char
-genAlphaChar = elements $ ['a'..'z'] ++ ['A'..'Z']
 
 notEmpty :: String -> Bool
 notEmpty str = length str /= 0
@@ -29,7 +32,7 @@ testOddLines :: AlphaChar -> Bool
 testOddLines alchar = mod (kataLineCount alchar) 2 == 1
 
 correctLines :: AlphaChar -> Int
-correctLines (AlphaChar char) = (ord (toLower char) - ord 'a') * 2 + 1
+correctLines (AlphaChar char) = (ord (toUpper char) - ord 'A') * 2 + 1
 
 testCorrectLines :: AlphaChar -> Bool
 testCorrectLines alchar = kataLineCount alchar == correctLines alchar
@@ -40,12 +43,18 @@ testReverseLines alchar = let kl = kataLines alchar in kl == reverse kl
 testLinesNonEmpty :: AlphaChar -> Bool
 testLinesNonEmpty alchar = all notEmpty (kataLines alchar)
 
+testLinesCorrectChar :: AlphaChar -> Bool
+testLinesCorrectChar alchar@(AlphaChar char) = all foo (zip (kataLines alchar) ['A'..(toUpper char)])
+  where
+    foo (l, c) = head l == c
+
 tests :: [AlphaChar -> Bool]
 tests = [ testNonEmpty
         , testOddLines
         , testCorrectLines
         , testReverseLines
         , testLinesNonEmpty
+        , testLinesCorrectChar
         ]
 
 main :: IO ()
